b92673bea769d02e99b211e4cb586b6116fb1f69
Olga <olga.slepova87@gmail.com>
2025-10-27 23:14:53 +0100

    feat: implement getSumOfDigits function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -296,8 +296,13 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  const str = String(num);
+  let sum = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    sum += Number(str[i]);
+  }
+  return sum;
 }
 
 /**
